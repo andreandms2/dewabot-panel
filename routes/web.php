bf50c31b44721b7cc75dd3a47472eb3ca0932d63
andreandms2 <brobrosiber@gmail.com>
2025-09-01 10:20:02 +0700

    fix: correct use statement order in web routes

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController; // <-- DIPINDAHKAN KE ATAS
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Di sinilah Anda bisa mendaftarkan rute web untuk aplikasi Anda. Rute-rute
+| ini dimuat oleh RouteServiceProvider dan semuanya akan
+| ditetapkan ke grup middleware "web". Buat sesuatu yang hebat!
+|
+*/
+
+// Rute ini akan melayani app.dewabot.com
 Route::get('/', function () {
     return view('welcome');
 });
 
-
-use App\Http\Controllers\Admin\DashboardController;
-
-// Rute untuk Panel Admin
+// Rute ini HANYA akan melayani admin.dewabot.com
 Route::domain('admin.dewabot.com')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
 });
